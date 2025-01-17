7eeaf7dcb1177a52a3df4c944ef501752705686b
Juanes0302 <Jcastillo@sipcolombia.net>
2025-01-17 09:12:35 -0500

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
